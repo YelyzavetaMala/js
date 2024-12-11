2638ad298def9e6fac810e5a88069479dc89a4d1
YelyzavetaMala <malaelizaveta678@gmail.com>
2024-12-11 19:44:54 +0200

    Lesson5 1/2

--- js/task-15.js
+++ js/task-15.js
@@ -1,1 +1,32 @@
+function createURL(domain) {
+  return function (url) {
+    return `https://${url}.${domain}`;
+  };
+}
+const comUrl = createURL("com");
+const comUa = createURL("ua");
+console.log(comUrl("google"));
+console.log(comUa("fb"));
 
+/*
+function abc() {
+  console.log("В функції");
+  console.log(this);
+}
+abc();
+document.querySelector("p").onclick = abc;
+
+function changeColor() {
+  console.log(this);
+  this.style.background = "green";
+}
+document.querySelector("div").onclick = changeColor;
+*/
+
+function changeColor() {
+  this.style.background = "green";
+}
+let user = document.querySelectorAll("div");
+user.forEach(function (element) {
+  element.onclick = changeColor;
+});
