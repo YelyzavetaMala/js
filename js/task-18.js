acced6e326791c0943652c061ef0d01ce355db39
YelyzavetaMala <malaelizaveta678@gmail.com>
2025-01-05 21:20:05 +0200

    Lesson 8 2/3

--- js/task-18.js
+++ js/task-18.js
@@ -7,3 +7,16 @@ let elements = document.querySelectorAll("ul>li:last-child");
 for (element of elements) {
   console.log(element);
 }
+
+let element = document.getElementsByTagName("div");
+console.log(element);
+
+let textHidden = document.getElementById("textHidden");
+textHidden.hidden = true;
+
+let message = document.getElementById("message");
+console.log(message.value);
+
+let text = document.querySelector(".text-message");
+console.log(text.textContent);
+text.textContent = "hello, olga";
