81045dbd1ac447e434734a9e74b2a0b67a0ea425
YelyzavetaMala <malaelizaveta678@gmail.com>
2025-01-06 20:31:49 +0200

    Lesson 8 3/3

--- js/task-18.js
+++ js/task-18.js
@@ -1,3 +1,4 @@
+/*
 let elem = document.getElementById("elem");
 
 elem.style.background = "green";
@@ -20,3 +21,58 @@ console.log(message.value);
 let text = document.querySelector(".text-message");
 console.log(text.textContent);
 text.textContent = "hello, olga";
+
+let btn = document.querySelector(".button");
+btn.style.backgroundColor = "red";
+
+
+let text = document.querySelector("#p_text");
+console.log(text.classList);
+console.log(text.classList.contains("red"));
+console.log(text.classList.remove("red"));
+text.classList.edd("new");
+console.log(text.classList);
+
+
+let image = document.querySelector(".image");
+console.log(image.hasAttribute("src"));
+console.log(image.getAttribute("src"));
+
+
+const item = document.createElement("a");
+item.href = "#";
+item.classList.add("btn");
+item.textContent = "3";
+
+const nav = document.querySelector(".nav");
+nav.appendChild(item);
+
+const heading = document.createElement("h1");
+heading.textContent = "Hello";
+const container = document.querySelector(".conteiner");
+container.insertBefore(heading, nav);
+
+nav.removeChild(item);
+item.remove();
+
+
+
+const text = document.querySelector(".text");
+const parent = document.querySelector(".parent");
+
+const clone = text.cloneNode(true);
+parent.appendChild(clone);
+
+
+
+const container = document.querySelector(".container");
+const text = `<p>Sorry</p>`;
+container.innerHTML += text;
+
+const list = document.querySelector("#list");
+list.insertAdjacentHTML("beforebegin", "<p>beforebegin</p>");
+list.insertAdjacentHTML("afterbegin", "<li>afterbegin</li>");
+list.insertAdjacentHTML("beforebegin", "<li>beforebegin</li>");
+list.insertAdjacentHTML("afterbegin", "<p>afterbegin</p>");
+
+*/
