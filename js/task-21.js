d5102f0f2b8e7a5eacb78ecd9d3786bb94153d0c
YelyzavetaMala <malaelizaveta678@gmail.com>
2025-05-06 20:11:21 +0300

    task 1

---
+++ js/task-21.js
@@ -0,0 +1,28 @@
+let name = "Ivan";
+let city = name;
+
+console.log(city);
+
+let a = 0.1;
+let b = 0.2;
+let c = a + b;
+
+console.log(c);
+
+console.log(Math.max(20, 10, 50, 40));
+
+const message = "Welcome to Bahamas!";
+
+console.log(message.length);
+
+console.log(message.toUpperCase());
+
+const user = {};
+user.name = "Ivan";
+user.age = 25;
+user.city = "Lviv";
+
+console.log(user);
+delete user.city;
+
+console.log(user);
