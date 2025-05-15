0b7540ce874b94cbcc0aee228213d13ba1f05870
YelyzavetaMala <malaelizaveta678@gmail.com>
2025-05-15 22:59:52 +0300

    task 6

--- js/task-21.js
+++ js/task-21.js
@@ -61,3 +61,34 @@ function shop(productName, pricePerKg, quantityKg) {
 console.log(shop("banana", 30, 4.5));
 console.log(shop("cherry", 58, 1.3));
 console.log(shop("orange", 89, 3.4));
+
+class Abonent {
+  constructor(name, number) {
+    this._name = name;
+    this._number = number;
+  }
+
+  // setter для оновлення номера
+  set updateNumber(newNumber) {
+    this._number = newNumber;
+  }
+
+  // getter для виведення даних
+  get info() {
+    return `Ім'я: ${this._name}, Номер: ${this._number}`;
+  }
+}
+
+// Створення трьох абонентів
+const user1 = new Abonent("Оля", "0931234567");
+const user2 = new Abonent("Іван", "0677654321");
+const user3 = new Abonent("Марія", "0509876543");
+
+// Вивід даних
+console.log(user1.info);
+console.log(user2.info);
+console.log(user3.info);
+
+// Оновлення номера
+user2.updateNumber = "0660001122";
+console.log("Оновлений номер Івана:", user2.info);
