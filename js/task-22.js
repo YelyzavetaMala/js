1c25b0e0fd33ee9c0fa323fa40e8b38a74e29ef4
YelyzavetaMala <malaelizaveta678@gmail.com>
2025-08-18 20:23:48 +0300

    task 22 1/2

--- js/task-22.js
+++ js/task-22.js
@@ -11,3 +11,39 @@ function multiply1(x, y, z) {
 
 const result = multiply1(2, 3, 5);
 console.log(result);
+
+function getSubscriptionPrice(type) {
+  switch (type) {
+    case "starter":
+      return "0";
+      break;
+    case "professional":
+      return "20";
+      break;
+    case "organization":
+      return "50";
+      break;
+    default:
+      return "Invalid subscription type!";
+  }
+}
+
+// Глобальна змінна
+const value = "I'm a global variable";
+
+if (true) {
+  // Можна звернутися до глобальної змінної
+  console.log(value); // "I'm a global variable"
+}
+
+// Можна звернутися до глобальної змінної
+console.log(value); // "I'm a global variable"
+
+if (true) {
+  // Локальна змінна
+  const value = "I'm a local variable";
+  console.log(value); // "I'm a local variable"
+}
+
+// Помилка: локальну змінну не видно за межами блоку
+console.log(value); // ReferenceError: value is not defined
