549dde7800b47876d96b28691a69689dd2d6a0c0
YelyzavetaMala <malaelizaveta678@gmail.com>
2025-09-10 17:54:28 +0300

    Update task-22

--- js/task-22.js
+++ js/task-22.js
@@ -199,3 +199,15 @@ const credentials = {
   [emailInputName]: "henry.carter@aptmail.com",
   [passwordInputName]: "jqueryismyjam",
 };
+
+const book = {
+  author: "Bernard Cornwell",
+  genres: ["historical prose", "adventure"],
+  rating: 8.38,
+};
+const keys = Object.keys(book);
+
+for (const key of keys) {
+  console.log(key); // Ключ
+  console.log(book[key]); // Значення властивості
+}
