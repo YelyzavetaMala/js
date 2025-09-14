e0ee89a2fdcde379a381007850e65bb94db75120
YelyzavetaMala <malaelizaveta678@gmail.com>
2025-09-14 19:58:59 +0300

    Update task-22

--- js/task-22.js
+++ js/task-22.js
@@ -211,3 +211,25 @@ for (const key of keys) {
   console.log(key); // Ключ
   console.log(book[key]); // Значення властивості
 }
+
+for (const book of books) {
+  console.log(book); // Об'єкт книги
+  console.log(book.title); // Назва
+  console.log(book.author); // Автор
+  console.log(book.rating); // Рейтинг
+}
+
+const colors = [
+  { hex: "#f44336", rgb: "244,67,54" },
+  { hex: "#2196f3", rgb: "33,150,243" },
+  { hex: "#4caf50", rgb: "76,175,80" },
+  { hex: "#ffeb3b", rgb: "255,235,59" },
+];
+
+const hexColors = [];
+const rgbColors = [];
+
+for (const color of colors) {
+  hexColors.push(color.hex);
+  rgbColors.push(color.rgb);
+}
